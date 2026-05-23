471f21d9cf19d17c184ce06770db630dae57df09
Shikhar Awasthi <57915075+ShikharAwasthi27@users.noreply.github.com>
2026-05-23 16:57:31 +0530

    reranker

---
+++ rag/reranker.py
@@ -0,0 +1,24 @@
+from sentence_transformers import CrossEncoder
+
+
+class Reranker:
+
+    def __init__(self):
+
+        self.model = CrossEncoder(
+            'cross-encoder/ms-marco-MiniLM-L-6-v2'
+        )
+
+    def rerank(self, query, docs):
+
+        pairs = [[query, doc.page_content] for doc in docs]
+
+        scores = self.model.predict(pairs)
+
+        ranked = sorted(
+            zip(scores, docs),
+            key=lambda x: x[0],
+            reverse=True
+        )
+
+        return [doc for score, doc in ranked[:5]]
